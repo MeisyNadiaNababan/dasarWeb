a1adeed5c507199145702c31c9fb216932af3398
MeisyNadiaNababan <meisynadianababan@gmail.com>
2024-09-18 14:45:11 +0700

    update

---
+++ JS 04 - PHP/operator.php
@@ -0,0 +1,18 @@
+<?php
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Hasil penjumlahan {$a} + {$b} = {$hasilTambah} <br>";
+echo "Hasil pengurangan {$a} - {$b} = {$hasilKurang} <br>";
+echo "Hasil perkalian {$a} * {$b} = {$hasilKali} <br>";
+echo "Hasil pembagian {$a} / {$b} = {$hasilBagi} <br>";
+echo "Sisa bagi {$a} % {$b} = {$sisaBagi} <br>";
+echo "Hasil pangkat {$a} ** {$b} = {$pangkat} <br>";
+?>
