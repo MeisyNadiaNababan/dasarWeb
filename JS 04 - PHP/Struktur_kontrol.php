b218a8de46b069c4d2182e3cac6b6fe826c9fd27
MeisyNadiaNababan <meisynadianababan@gmail.com>
2024-09-18 16:27:38 +0700

    update

---
+++ JS 04 - PHP/Struktur_kontrol.php
@@ -0,0 +1,34 @@
+<?php
+$nilaiNumerik = 92;
+
+if ($nilaiNumerik >= 90 && $nilaiNumerik <= 100) {
+    echo "Nilai huruf: A<br>";
+} elseif ($nilaiNumerik >= 80 && $nilaiNumerik < 90) {
+    echo "Nilai huruf: B<br>";
+} elseif ($nilaiNumerik >= 70 && $nilaiNumerik < 80) {
+    echo "Nilai huruf: C<br>";
+} elseif ($nilaiNumerik < 70) {
+    echo "Nilai huruf: D<br>";
+}
+
+$jarakSaatIni = 0;
+$jarakTarget = 500;
+$peningkatanHarian = 30;
+$hari = 0;
+
+while ($jarakSaatIni < $jarakTarget) {
+    $jarakSaatIni += $peningkatanHarian;
+    $hari++;
+}
+echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer.<br><br>";
+
+$jumlahLahan = 10;
+$tanamanPerLahan = 5;
+$buahPerTanaman = 10;
+$jumlahBuah = 0;
+
+for ($i = 1; $i <= $jumlahLahan; $i++) {
+    $jumlahBuah += ($tanamanPerLahan * $buahPerTanaman);
+}
+echo "Jumlah buah yang akan dipanen adalah: $jumlahBuah";
+?>
