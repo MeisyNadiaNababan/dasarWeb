7200e9554f54060c4c0c0a436addca7df634889a
MeisyNadiaNababan <meisynadianababan@gmail.com>
2024-09-25 10:26:00 +0700

    Praktikum 1.1

--- JS 05 - PHP 2/array_1.php
+++ JS 05 - PHP 2/array_1.php
@@ -6,7 +6,10 @@ $Listdosen=["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
 echo $Listdosen[2] . "<br>";
 echo $Listdosen[0] . "<br>";
 echo $Listdosen[1] . "<br>";
-
+echo "<h4>Menggunakan Loop</h4>";
+for ($i= 0;$i<count($Listdosen);$i++) {
+    echo $Listdosen[$i] . "<br>";
+}
 ?>
 </body>
 </htmL>
