e06f85c42a04709889ba343b23fb3dae301fbea5
MeisyNadiaNababan <meisynadianababan@gmail.com>
2024-09-25 12:18:30 +0700

    JS 05 - P7 L1

--- JS 05 - PHP 2/string1.php
+++ JS 05 - PHP 2/string1.php
@@ -1,5 +1,4 @@
 <?php
-
 $loremIpsum = "Lorem ipsum dolor sit amet consectetur adipisicing elit.
     Voluptatem reprehenderit nobis veritatis commodi fugiat molestias
     impedit unde ipsum voluptatum, corrupti minus sit excepturi nostrum
