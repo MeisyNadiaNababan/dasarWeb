b956a4b0c3a35390832f074d81445805a3b5ab53
MeisyNadiaNababan <meisynadianababan@gmail.com>
2024-10-09 12:46:03 +0700

    JS07_P4 L6

--- JS 07 - PHP_FORM/html_aman.php
+++ JS 07 - PHP_FORM/html_aman.php
@@ -10,7 +10,9 @@
         <input type="text" name="nama" id="nama" required>
         <br><br>
         
-    
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" required>
+        <br><br>
 
         <input type="submit" name="submit" value="Submit">
     </form>
@@ -22,7 +24,19 @@
         $nama = htmlspecialchars($nama, ENT_QUOTES, 'UTF-8');
         
         echo "<h3>Nama yang diterima:</h3>";
-      echo htmlspecialchars($nama, ENT_QUOTES, 'UTF-8');
+        echo htmlspecialchars($nama, ENT_QUOTES, 'UTF-8');
+        
+        // Memeriksa apakah input adalah email yang valid
+        $email = $_POST['email'];
+        
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // Lanjutkan dengan pengolahan email yang aman
+            echo "<h3>Email yang diterima:</h3>";
+            echo htmlspecialchars($email, ENT_QUOTES, 'UTF-8');
+        } else {
+            // Tangani input yang tidak valid
+            echo "<h3>Email tidak valid!</h3>";
+        }
     }
     ?>
 </body>
