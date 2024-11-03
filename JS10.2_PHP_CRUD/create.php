349b4506c7186785f4e35120239a8553f05845fa
MeisyNadiaNababan <meisynadianababan@gmail.com>
2024-11-03 16:20:30 +0700

    P5.L6

--- JS10.2_PHP_CRUD/create.php
+++ JS10.2_PHP_CRUD/create.php
@@ -1,23 +1,21 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <title>Tambah Data Anggota</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
-
 <body>
     <div class="container mt-4">
         <h2>Tambah Data Anggota</h2>
 
         <form action="proses.php?aksi=tambah" method="post">
             <div class="form-group">
-                <label for="nama">Nama: </label>
+                <label for="nama">Nama:</label>
                 <input type="text" class="form-control" name="nama" id="nama" required>
             </div>
 
             <div class="form-group">
-                <label for="jenis_kelamin">Jenis Kelamin: </label>
+                <label for="jenis_kelamin">Jenis Kelamin:</label>
                 <div class="form-check">
                     <input type="radio" class="form-check-input" name="jenis_kelamin" value="L" id="laki" required>
                     <label class="form-check-label" for="laki">Laki-laki</label>
@@ -34,7 +32,7 @@
             </div>
 
             <div class="form-group">
-                <label for="no_telp">No. Telp: </label>
+                <label for="no_telp">No. Telp:</label>
                 <input type="text" class="form-control" name="no_telp" id="no_telp" required>
             </div>
 
@@ -48,5 +46,4 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
-
 </html>
