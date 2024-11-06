218bb027cd543f176651d03c5e1a333c33ecbbc0
MeisyNadiaNababan <meisynadianababan@gmail.com>
2024-11-06 15:47:25 +0700

    P5.L4

--- JS11_PHP_Web_Dinamis_Struktural/fungsi/tambah.php
+++ JS11_PHP_Web_Dinamis_Struktural/fungsi/tambah.php
@@ -17,7 +17,6 @@ if (!empty($_SESSION['username'])) {
         }
 
         header("Location: ./index.php?page=jabatan");
-        
     } elseif (!empty($_GET['anggota'])) {
         $username = antiinjection($koneksi, $_POST['username']);
         $password = antiinjection($koneksi, $_POST['password']);
