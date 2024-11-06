42b66e8f7d4e966a434431c484190a768ba33143
MeisyNadiaNababan <meisynadianababan@gmail.com>
2024-11-06 15:32:00 +0700

    P5.L6

--- JS11_PHP_Web_Dinamis_Struktural/fungsi/hapus.php
+++ JS11_PHP_Web_Dinamis_Struktural/fungsi/hapus.php
@@ -16,17 +16,22 @@ if (!empty($_SESSION['username'])) {
         }
 
         header("Location: ./index.php?page=jabatan");
-    } else if (!empty($_GET['anggota'])) {
+    } elseif (!empty($_GET['anggota'])) {
         $id = antiinjection($koneksi, $_GET['id']);
-        $query = "DELETE FROM user WHERE user_id = '$id'";
-        
+        $query = "DELETE FROM user WHERE id = '$id'";
+
         if (mysqli_query($koneksi, $query)) {
-            pesan('success', "Anggota Telah Terhapus.");
+            $query2 = "DELETE FROM anggota WHERE user_id = '$id'";
+            if (mysqli_query($koneksi, $query2)) {
+                pesan('success', "Anggota Telah Terhapus.");
+            } else {
+                pesan('warning', "Data Login Terhapus Tetapi Data Anggota Tidak Terhapus Karena: " . mysqli_error($koneksi));
+            }
         } else {
-            pesan('danger', "Data Login Terhapus Tetapi Data Anggota Todak Terhapus Karena: " . mysqli_error($koneksi));
+            pesan('danger', "Anggota Tidak Terhapus Karena: " . mysqli_error($koneksi));
         }
 
-        header("Location: ./index.php?page=anggota");
+        header("Location: ../index.php?page=anggota");
     }
 }
 ?>
