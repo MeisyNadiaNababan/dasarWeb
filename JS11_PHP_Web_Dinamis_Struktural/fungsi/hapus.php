dbb1c34f541d96e1060553eaed93e797abcb5830
MeisyNadiaNababan <meisynadianababan@gmail.com>
2024-11-06 16:45:06 +0700

    P5.L6

--- JS11_PHP_Web_Dinamis_Struktural/fungsi/hapus.php
+++ JS11_PHP_Web_Dinamis_Struktural/fungsi/hapus.php
@@ -4,29 +4,29 @@ if (!empty($_SESSION['username'])) {
     require '../config/koneksi.php';
     require '../fungsi/pesan_kilat.php';
     require '../fungsi/anti_injection.php';
-
     if (!empty($_GET['jabatan'])) {
         $id = antiinjection($koneksi, $_GET['id']);
         $query = "DELETE FROM jabatan WHERE id = '$id'";
-        
         if (mysqli_query($koneksi, $query)) {
-            pesan('success', "Jabatan Telah Terhapus.");
+            pesan('success', "Jabatan telah terhapus");
         } else {
-            pesan('danger', "Jabatan Tidak Terhapus Karena: " . mysqli_error($koneksi));
+            pesan('danger', "Jabatan tidak terhapus karena : " . mysqli_error($koneksi));
         }
-
-        header("Location: ./index.php?page=jabatan");
-    } else if (!empty($_GET['anggota'])) {
+        header("Location: ../index.php?page=jabatan");
+    } elseif (!empty($_GET['anggota'])){
         $id = antiinjection($koneksi, $_GET['id']);
-        $query = "DELETE FROM user WHERE user_id = '$id'";
-        
+        $query = "DELETE FROM user WHERE id = '$id'";
         if (mysqli_query($koneksi, $query)) {
-            pesan('success', "Anggota Telah Terhapus.");
+            $query2 = "DELETE FROM anggota WHERE user_id = '$id'";
+            if (mysqli_query($koneksi, $query)){
+                pesan('success', "Anggota telah terhapus");
+            } else {
+                pesan('warning', "Data login terhapus tetapi data anggota tidak terhapus karena : " . mysqli_error($koneksi));
+            }
         } else {
-            pesan('danger', "Data Login Terhapus Tetapi Data Anggota Todak Terhapus Karena: " . mysqli_error($koneksi));
+            pesan('danger', "Anggota tidak terhapus karena : " . mysqli_error($koneksi));
         }
-
-        header("Location: ./index.php?page=anggota");
+        header("Location: ../index.php?page=anggota");
     }
 }
 ?>
