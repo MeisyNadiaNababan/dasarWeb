282e47616ebc415df6280fc3a9ddaf4174066a9d
MeisyNadiaNababan <meisynadianababan@gmail.com>
2024-11-13 13:09:42 +0700

    P1.L6

--- JS12_PHP_OOP/inheritance.php
+++ JS12_PHP_OOP/inheritance.php
@@ -5,11 +5,9 @@ class Animal {
     public function __construct($name) {
         $this->name = $name;
     }
-
     public function eat() {
         echo $this->name . " is eating.<br>";
     }
-
     public function sleep() {
         echo $this->name . " is sleeping.<br>";
     }
